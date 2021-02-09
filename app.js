5e1d9770376a9df5b1a1eecb240d2770174701c4
Farouk <faroukkisuule55@gmail.com>
2021-02-08 21:59:54 -0500

    I have added a node module called ejs that will enable me organise my application and make things simpler for the user

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(req,res) {
+    // res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.render('index');
+});
+
+// app.get('/notes', function(req,res) {
+//     res.sendFile(path.join(__dirname, "./views/index.html"));
+// });
+
+app.get('/about', (req,res)=>{
+    // res.sendFile(path.join(__dirname, "./views/blog.html"));
+    res.render('about');
+});
+
+app.get('/blog/create', (req,res)=>{
+    res.render('blog');
+});
+
+// app.get('/about', function(req,res) {
+//     res.sendFile(path.join(__dirname, "./index.html"));
+// });
+
+app.delete('api/notes/:id', (req,res)=> {
+    res.sendFile(path.join(__dirname, "./views/index.html"));
+});
+
+app.use((req,res) => {
+    // res.status(404).sendFile(path.join(__dirname, "./views/404.html"));
+    res.status(404).render('404');
+})
+
+app.listen(PORT, () => {
+    console.log("Listening to Port 3000...")
+});
+
