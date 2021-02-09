d8e3ff560d9f417c72a81a2fc624d4f3c0ff10c5
Farouk <faroukkisuule55@gmail.com>
2021-02-09 06:48:30 -0500

    Updated all files and made some changes with the javascript file

--- app.js
+++ app.js
@@ -3,27 +3,28 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
-const PORT = 3000;
+// const PORT = 3000;
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 
 app.get('/', function(req,res) {
-    // res.sendFile(path.join(__dirname, "./public/index.html"));
-    res.render('index');
+    res.sendFile(path.join(__dirname, "./views/index.html"));
+    // res.render('index');
 });
 
 // app.get('/notes', function(req,res) {
 //     res.sendFile(path.join(__dirname, "./views/index.html"));
 // });
 
-app.get('/about', (req,res)=>{
+// app.get('/about', (req,res)=>{
     // res.sendFile(path.join(__dirname, "./views/blog.html"));
-    res.render('about');
-});
+    // res.render('about');
+// });
 
 app.get('/blog/create', (req,res)=>{
-    res.render('blog');
+    res.sendFile(path.join(__dirname, "./views/blog.html"));
+    // res.render('blog');
 });
 
 // app.get('/about', function(req,res) {
@@ -35,11 +36,13 @@ app.delete('api/notes/:id', (req,res)=> {
 });
 
 app.use((req,res) => {
-    // res.status(404).sendFile(path.join(__dirname, "./views/404.html"));
-    res.status(404).render('404');
+    res.status(404).sendFile(path.join(__dirname, "./views/404.html"));
+    // res.status(404).render('404');
 })
 
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log("Listening to Port 3000...")
+    console.log("Listening to Port...")
 });
 
